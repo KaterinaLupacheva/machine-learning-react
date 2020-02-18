3f98b766ac5b106331e2f50a6acd3b2e0ad25411
Katerina <katerinalupacheva@gmail.com>
2020-02-18 14:23:50 +0300

    Factory icon added

--- src/components/menu/menu.component.js
+++ src/components/menu/menu.component.js
@@ -4,6 +4,7 @@ import {ReactComponent as TelecomIcon} from '../../assets/page1-icons/telecom-ic
 import {ReactComponent as CartIcon} from '../../assets/page1-icons/cart-icon.svg';
 import {ReactComponent as HealthcareIcon} from '../../assets/page1-icons/healthcare-icon.svg';
 import {ReactComponent as BankingIcon} from '../../assets/page1-icons/banking-icon.svg';
+import {ReactComponent as FactoryIcon} from '../../assets/page1-icons/factory-icon.svg';
 import './menu.styles.scss';
 
 const Menu = () => (
@@ -23,6 +24,10 @@ const Menu = () => (
         <div className='menu-item'>
             <BankingIcon className='icon'/>
             <MenuButton>{'Banking'}</MenuButton>
+        </div>
+        <div className='menu-item'>
+            <FactoryIcon className='icon'/>
+            <MenuButton>{'Supply Chain'}</MenuButton>
         </div>
     </div>
 );
