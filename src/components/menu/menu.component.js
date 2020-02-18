0713e9563e653063b17591eb233804990bfe8f34
Katerina <katerinalupacheva@gmail.com>
2020-02-18 14:17:34 +0300

    Banking icon added

--- src/components/menu/menu.component.js
+++ src/components/menu/menu.component.js
@@ -3,21 +3,26 @@ import MenuButton from '../menu-button/menu-button.component';
 import {ReactComponent as TelecomIcon} from '../../assets/page1-icons/telecom-icon.svg';
 import {ReactComponent as CartIcon} from '../../assets/page1-icons/cart-icon.svg';
 import {ReactComponent as HealthcareIcon} from '../../assets/page1-icons/healthcare-icon.svg';
+import {ReactComponent as BankingIcon} from '../../assets/page1-icons/banking-icon.svg';
 import './menu.styles.scss';
 
 const Menu = () => (
     <div className='menu-container'>
         <div className='menu-item'>
-            <TelecomIcon className='telecom-icon'/>
+            <TelecomIcon className='icon'/>
             <MenuButton>{'Telecom'}</MenuButton>
         </div>
         <div className='menu-item'>
-            <CartIcon className='cart-icon'/>
+            <CartIcon className='icon'/>
             <MenuButton>{'Retail'}</MenuButton>
         </div>
         <div className='menu-item'>
-            <HealthcareIcon className='healthcare-icon'/>
+            <HealthcareIcon className='icon'/>
             <MenuButton>{'Healthcare'}</MenuButton>
+        </div>
+        <div className='menu-item'>
+            <BankingIcon className='icon'/>
+            <MenuButton>{'Banking'}</MenuButton>
         </div>
     </div>
 );
