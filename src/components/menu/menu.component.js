f63ca5fe4f67fd3f168053847c494adf4fc8490e
Katerina <katerinalupacheva@gmail.com>
2020-02-18 13:48:17 +0300

    Cart icon added

--- src/components/menu/menu.component.js
+++ src/components/menu/menu.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuButton from '../menu-button/menu-button.component';
 import {ReactComponent as TelecomIcon} from '../../assets/page1-icons/telecom-icon.svg';
+import {ReactComponent as CartIcon} from '../../assets/page1-icons/cart-icon.svg';
 import './menu.styles.scss';
 
 const Menu = () => (
@@ -8,6 +9,10 @@ const Menu = () => (
         <div className='menu-item'>
             <TelecomIcon className='telecom-icon'/>
             <MenuButton>{'Telecom'}</MenuButton>
+        </div>
+        <div className='menu-item'>
+            <CartIcon className='cart-icon'/>
+            <MenuButton>{'Retail'}</MenuButton>
         </div>
     </div>
 );
