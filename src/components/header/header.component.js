5f8dd7d879cd0ce37550333c1e2da567697bea4f
Katerina <katerinalupacheva@gmail.com>
2020-02-18 12:39:16 +0300

    Slogan component added

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Slogan from '../slogan/slogan.component';
 import './header.styles.scss';
 
 import homeIcon from '../../assets/page1-icons/home-icon.png';
@@ -7,6 +8,7 @@ import homeIcon from '../../assets/page1-icons/home-icon.png';
 const Header = () => {
     return(
         <div className='header'>
+            <Slogan />
             <div className='options'>
                 <Link className='option' to='/'>
                     <img src={homeIcon} alt='home-icon' />
---
+++ src/components/slogan/slogan.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './slogan.styles.scss';
+
+const Slogan = () => (
+    <div className='slogan-container'>
+        <div className='nebula'>
+            {'Nebula CI'}
+        </div>
+        <div className='moto'>
+            {'a galaxy full of benefits & intelegence'}
+        </div>
+    </div>
+);
+
+export default Slogan;
