9244bd2a863f404d370cc4c8a27246d148613f06
Katerina <katerinalupacheva@gmail.com>
2020-02-18 13:03:43 +0300

    Gear icon added

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Slogan from '../slogan/slogan.component';
 import {ReactComponent as HomeIcon} from '../../assets/page1-icons/home-icon.svg';
+import {ReactComponent as GearIcon} from '../../assets/page1-icons/gear-icon.svg';
 import './header.styles.scss';
 
 const Header = () => {
@@ -11,6 +12,9 @@ const Header = () => {
             <div className='options'>
                 <Link className='option' to='/'>
                     <HomeIcon className='home-icon'/>
+                </Link>
+                <Link className='option' to='/'>
+                    <GearIcon className='gear-icon' />
                 </Link>
             </div>
         </div>
