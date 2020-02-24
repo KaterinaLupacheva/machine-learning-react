f5f3216beca08d0e401633bfd3258eedb209c34b
Katerina <katerinalupacheva@gmail.com>
2020-02-24 10:25:47 +0300

    Background added

--- src/components/sidebar/sidebar.component.js
+++ src/components/sidebar/sidebar.component.js
@@ -5,13 +5,17 @@ import MENU_OPTIONS from './menu-options.data';
 import './sidebar.styles.scss';
 
 const Sidebar = () => (
-  <div className="sidebar-container">
-    <ul>
-      {MENU_OPTIONS.map(option => (
-        <SidebarOption key={option.id} icon={option.icon} name={option.name} />
-      ))}
-    </ul>
-  </div>
+  <>
+    <div className="sidebar-container">
+      <header>Nebula CI</header>
+      <ul>
+        {MENU_OPTIONS.map(option => (
+          <SidebarOption key={option.id} icon={option.icon} name={option.name} />
+        ))}
+      </ul>
+    </div>
+    <section></section>
+  </>
 );
 
 export default Sidebar;
