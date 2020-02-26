e37bb937ebd9c6e5cd8cb15ad487271cc5f0adc4
Katerina <katerinalupacheva@gmail.com>
2020-02-26 14:39:10 +0300

    Two icons for the second page and animation added

--- src/components/ml-menu/ml-menu.data.js
+++ src/components/ml-menu/ml-menu.data.js
@@ -1,10 +1,16 @@
 import { ReactComponent as Icon1 } from '../../assets/page2-icons/icon1.svg';
+import { ReactComponent as Icon2 } from '../../assets/page2-icons/icon2.svg';
 
 const ML_MENU_OPTIONS = [
   {
     id: 1,
     icon: Icon1,
     name: 'Demand Prediction',
+  },
+  {
+    id: 2,
+    icon: Icon2,
+    name: 'Price Formation',
   },
 ];
 
